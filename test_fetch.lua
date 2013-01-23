39c199395e9654f158f190f8498f3a479570288e
Michal Kottman <michal.kottman@gmail.com>
2013-01-23 17:55:17 +0100

    Make test_fetch.lua more generic

--- test_fetch.lua
+++ test_fetch.lua
@@ -3,9 +3,12 @@ package.cpath = package.cpath .. ';b/?.so'
 
 require 'git'
 
+local repoUrl = arg[1] or 'git://github.com/mkottman/lua-git.git'
+local ref = arg[2] or 'refs/heads/master'
+
 os.execute('rm -rf /tmp/test.git')
 os.execute('rm -rf /tmp/extracted')
 
 R = git.repo.create('/tmp/test.git')
-local pack, sha = git.protocol.fetch('git://github.com/mkottman/lua-git.git', R, 'refs/heads/master')
+local pack, sha = git.protocol.fetch(repoUrl, R, ref)
 R:checkout(sha, '/tmp/extracted')
